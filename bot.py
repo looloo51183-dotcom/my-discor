af284c53c1e25dddda40125931f5ccf9d6a555d1
looloo51183-dotcom <looloo51183@gmail.com>
2026-09-06 16:52:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,26 @@
 
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        await ctx.author.voice.channel.connect()
+    else:
+        await ctx.send("ادخلي روم صوتي أول.")
+
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+
+bot.run("YOUR_TOKEN_HERE")
